29adf0634dda0a0eb55a866cf9f7c3588e3db3cf
Zeha Irawan <jangkaringbumi@gmail.com>
2018-11-28 11:08:05 +0700

    finishing pig latin, book titles & timer

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,12 @@
 #write your code here
+def translate(phrase)
+    phrase.split(" ").map { |word| rearrange(word) }.join(" ") 
+  end
+  
+  def rearrange(word)
+    match = word.match(/^((?:qu|[bcdfghjklmnpqrstvwxz])*)(.*)$/)
+    match ? match[2] + match[1] + "ay" : word
+  end
+
+#need to split it to individual letter , move the 1st letter to last
+#.join("")+"ay"
